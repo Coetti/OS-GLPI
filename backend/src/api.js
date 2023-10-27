ac39991f53b881e3a987878fbce6a16395527b89
Coetti <coettigabriel6@gmail.com>
2023-10-27 17:00:47 -0300

    adicionei quase todos os endpoints
    adicionei muita coisa

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,9 +1,7 @@
 const https = require('https');
 const express = require('express');
 const axios = require('axios');
-const fs = require('fs');
 const cors = require('cors');
-
 
 const ip = "localhost";
 const initSessionPath = "/glpi/apirest.php/initSession";
@@ -12,9 +10,9 @@ const getEntitiesPath = "/glpi/apirest.php/getMyEntities/?is_recursive=true";
 const getEntitiesURL = `https://${ip}${getEntitiesPath}`;
 
 const contentType = 'application/json';
-const auth = 'user_token cKbvdbpK11lFG2Hgj6THf9ohUPP2dFjd1t074XfH'
-const appToken = '8r40UKACsvbA2dtGkEcz4s3ePkkHGs0ayfFSvbJa'
-var sessionToken = null;
+const auth = 'user_token cKbvdbpK11lFG2Hgj6THf9ohUPP2dFjd1t074XfH';
+const appToken = '8r40UKACsvbA2dtGkEcz4s3ePkkHGs0ayfFSvbJa';
+let sessionToken; // Defina sessionToken como uma variável global
 
 
 const agent = new https.Agent({
@@ -24,16 +22,16 @@ const agent = new https.Agent({
 const app = express();
 
 app.use(cors());
+app.use(express.json()); // Middleware para analisar o corpo da solicitação como JSON
 
-app.get('/initSession', async (req, res) => {
-
+// Middleware para inicializar a sessão
+app.use(async (req, res, next) => {
   const initHeaders = {
     'Content-Type': contentType,
     'Authorization': auth,
-    'App-Token': '8r40UKACsvbA2dtGkEcz4s3ePkkHGs0ayfFSvbJa'
+    'App-Token': appToken
   };
 
-  
   const options = {
     method: 'GET',
     headers: initHeaders,
@@ -47,30 +45,32 @@ app.get('/initSession', async (req, res) => {
     });
     const data = response.data;
     sessionToken = data.session_token;
-    console.log('this is the sessions token ' + sessionToken)
-    res.send(response.data); //remover
+    console.log('this is the sessions token ' + sessionToken);
+    next(); // Chame next() para continuar com a próxima rota
   } catch (error) {
+    console.error(error.message);
     res.status(500).send(error.message);
   }
 });
 
-var headers = {
-  'Content-Type': 'application/json',
-  'Session-Token': sessionToken,
-  'App-Token': '8r40UKACsvbA2dtGkEcz4s3ePkkHGs0ayfFSvbJa'
-};
+// Rota para buscar clientes
+app.get("/clientes", async (req, res) => {
+  
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
 
-app.get("/clientes", async (req,res) =>{
+  console.log(headers); // Certifique-se de que headers contenha o sessionToken
 
-console.log(headers)
-
-const options = {
+  const options = {
     method: 'GET',
     headers: headers,
     httpsAgent: agent
-};
+  };
 
-  try{
+  try {
     const response = await axios.request({
       url: getEntitiesURL,
       ...options
@@ -82,21 +82,251 @@ const options = {
     const extractedNames = clientes.map((cliente) => {
       const parts = cliente.name.split('&#62;');
       if (parts.length > 1) {
-        return parts[1].trim(); // Obtenha a segunda parte e remova espaços em branco extras
+        return parts[1].trim();
       } else {
-        return cliente.name; // Se não houver "&#62;", mantenha o nome original
+        return cliente.name;
       }
     });
 
     console.log(extractedNames);
+    console.log("killing the session...")
+    killSession();
     res.send(extractedNames);
-
-  }catch(error){
+  } catch (error) {
+    console.log("killing the session...")
+    killSession();
+    console.error(error.message);
     res.status(500).send(error.message);
   }
-
 });
 
-app.listen(8081, function(){
-  console.log("Node API conectada com sucesso!!")
+app.get('/getItems', async (req, res) => {
+  const { itemType } = req.query; // Obtém o valor do parâmetro 'itemType' da URL
+
+  const getItemsPath = `/glpi/apirest.php/${itemType}`;
+  const getItemsURL = `https://${ip}${getItemsPath}`;
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'GET',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try {
+    const response = await axios.request({
+      url: getItemsURL,
+      ...options
+    });
+
+    const responseBody = response.data; // Obter apenas o corpo da resposta
+    console.log("killing the session...")
+    killSession();
+    res.json(responseBody); // Enviar o corpo da resposta como JSON
+
+  } catch (error) {
+    console.log("killing the session...")
+    killSession();
+    console.error(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+app.get('/getTickets', async (req, res) => {
+  
+  const ticketsPath = "/glpi/apirest.php/Ticket"
+  const ticketsUrl = `https://${ip}${ticketsPath}`
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'GET',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try{
+    const response = await axios.request({
+      url: ticketsUrl,
+      ...options
+    });
+    console.log(response.data);
+    console.log("killing the session...")
+    killSession();
+    res.send(response.data);
+  }catch(error){
+    console.log("killing the session...")
+    killSession();
+    res.status(500).send(error.message);
+  }
+});
+
+function killSession(){
+
+  const killSessionPath = "/glpi/apirest.php/killSession"
+  const killSessionUrl = `https://${ip}${killSessionPath}`
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'GET',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try{
+    const killed = axios.request({
+      url: killSessionUrl,
+      ...options
+    })
+    return console.log(`Session Killed! ${sessionToken}`);
+  }catch(error){
+    console.log("Unexpected error!")
+  }
+} 
+
+app.post('/addCliente', async (req, res) => {
+  const dadosJSON = req.body
+  console.log(dadosJSON)
+  const addEntityPath = "/glpi/apirest.php/Entity";
+  const addEntityUrl = `https://${ip}${addEntityPath}`;
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try {
+    const response = await axios.post(addEntityUrl, dadosJSON, options);
+    const responseBody = response.data;
+    console.log("killing the session...")
+    killSession();
+    res.json(responseBody);
+  } catch (error) {
+    console.log("killing the session...")
+    killSession();
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/addItem', async (req, res) => {
+  const dadosJSON = req.body;
+  const itemType = dadosJSON.node.itemType;
+  console.log(dadosJSON + dadosJSON.node.itemType)
+  const addItemPath = `/glpi/apirest.php/${itemType}`;
+  const addItemUrl = `https://${ip}${addItemPath}`;
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    httpsAgent: agent
+  };
+  console.log(dadosJSON.input);
+
+  try {
+    const response = await axios.post(addItemUrl, dadosJSON, options);
+    const responseBody = response.data;
+    console.log("killing the session...")
+    killSession();
+    res.json(responseBody);
+  } catch (error) {
+    console.log("killing the session...")
+    killSession();
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/createTicket', async (req,res) => {
+
+  const dadosJSON = req.body
+
+  const createTicketPath = `/glpi/apirest.php/Ticket`;
+  const createTicketUrl = `https://${ip}${createTicketPath}`;
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try{
+    const response = await axios.post(createTicketUrl, dadosJSON, options)
+    const responseBody = response.data
+    console.log("killing the session...")
+    killSession();
+    res.json(responseBody)
+  }catch(error){
+    console.log("killing the session...")
+    killSession();
+    res.status(500).send(error.message)
+  }
+})
+
+app.post('/changeEntity', async (req,res) => {
+  const dadosJSON = req.body
+
+  const changeEntityPath = `/glpi/apirest.php/changeActiveEntities/`;
+  const changeEntityUrl = `https://${ip}${changeEntityPath}`;
+
+  const headers = {
+    'Content-Type': contentType,
+    'Session-Token': sessionToken,
+    'App-Token': appToken
+  };
+
+  const options = {
+    method: 'POST',
+    headers: headers,
+    httpsAgent: agent
+  };
+
+  try{
+
+    const response = await axios.post(changeEntityUrl, dadosJSON, options)
+    const responseBody = response.data
+    console.log("killing the session...")
+    killSession();
+    res.json(responseBody)
+  }catch(error){
+    console.log("killing the session...")
+    killSession();
+    res.status(500).send(responseBody)
+  }
+})
+
+// Inicie o servidor Express
+app.listen(8081, function () {
+  console.log("Node API conectada com sucesso!!");
 });
--- frontend/src/os-glpi/src/App.js
+++ frontend/src/os-glpi/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import DropdownList from './components/DropdownList';
+import './components-styles/DropdownList.css' ; 
 
 function App() {
   const [testeResponse, setTesteResponse] = useState(null);
   const [clientesResponse, setClientesResponse] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(''); // Inicialize com uma string vazia
 
   const handleTesteButtonClick = async () => {
     try {
@@ -18,11 +21,25 @@ function App() {
     try {
       const response = await fetch('http://localhost:8081/clientes');
       const data = await response.json();
+      const clientes = JSON.parse(response);
+      console.log(clientes);
       setClientesResponse(JSON.stringify(data, null, 2));
     } catch (error) {
       console.error('Erro na chamada para /clientes:', error);
     }
   };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
+  // Substitua yourOptionsArray com suas opções reais
+  const yourOptionsArray = [
+    { id: 1, label: 'Opção 1' },
+    { id: 2, label: 'Opção 2' },
+    { id: 3, label: 'Opção 3' },
+    // Adicione mais opções conforme necessário
+  ];
 
   return (
     <div>
@@ -42,8 +59,15 @@ function App() {
           <pre>{clientesResponse}</pre>
         </div>
       )}
+
+      <DropdownList
+        options={yourOptionsArray}
+        selectedOption={selectedOption}
+        onSelect={handleOptionSelect}
+      />
     </div>
   );
 }
 
 export default App;
+
